ca74c43baccddccfd24ef78d1782dff23c3b1028
rocionadal <rocioonadal@gmail.com>
2021-11-10 22:51:13 +0100

    Add simple validation + empty input(s) after submit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,10 +8,6 @@ const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
 
-  const addUserHandler = (event) => {
-    event.preventDefault();
-  };
-
   const usernameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
   }
@@ -20,13 +16,25 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   }
 
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    setEnteredUserName('');
+    setEnteredAge('');
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' onChange={usernameChangeHandler}/>
+        <input id='username' type='text' value={enteredUserName} onChange={usernameChangeHandler}/>
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' onChange={ageChangeHandler}/>
+        <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler}/>
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
